bdf0617f03837364d6bbb656677838e0ef703edf
Karan Sharma <karansharma2302578@gmail.com>
2026-01-06 16:00:38 +0530

    alternativeswap

---
+++ Array/alternativeSwap.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void alterNativeswap(int arr[], int size)
+{
+
+  for (int i = 0; i < size; i = i + 2)
+  {
+    if (i + 1 < size)
+    {
+      swap(arr[i], arr[i + 1]);
+    }
+  }
+}
+
+void printArray(int arr[], int size)
+{
+  for (int i = 0; i < size; i++)
+  {
+    cout << arr[i] << " ";
+  }
+}
+int main()
+{ // 0,1,2,3,4,5 size = 6
+  int arr[] = {5, 6, 7, 81, 2, 3};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  alterNativeswap(arr, size);
+  printArray(arr, size);
+}
